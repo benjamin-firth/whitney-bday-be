991da5f8685948802f62ddcc99b7c266b8810f7d
Ben Firth <bfirth93@gmail.com>
2020-09-22 17:24:48 -0600

    This might work!

--- server.js
+++ server.js
@@ -1,25 +1,51 @@
-require('dotenv').config();
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  Count = require('./api/models/whitneyModel'), //created model loading here
-  bodyParser = require('body-parser');
-  
-// mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Whitneydb', { useNewUrlParser: true, useUnifiedTopology: true}); 
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-var routes = require('./api/routes/whitneyRoutes'); //importing route
-routes(app); //register the route
+app.set('port', process.env.PORT || 3000);
 
 
-app.listen(port);
+app.locals.title = 'Whitneys Birthday';
+app.locals.count = [
+  { amount: 1, time: Date.now(), id: 666 },
+];
 
+app.get('/', (request, response) => {
+  response.send('Oh hey Pet Box');
+});
 
-console.log('todo list RESTful API server started on: ' + port);
+app.get('/count', (request, response) => {
+  const count = app.locals.count;
+
+  response.json({ count });
+});
+
+app.patch(`/count/:id`, (req, res) => { 
+  const oldCount = app.locals.count.find(val => val.id === Number(req.params.id));
+  oldCount.amount = req.body.amount;
+  oldCount.time = Date.now();
+  return res.json({ message: "Updated" });
+});
+
+app.post('/count', (request, response) => {
+  const id = Date.now();
+  const newCount = request.body;
+
+  for (let requiredParameter of ['amount', 'time']) {
+    if (!newCount[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, type: <String> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  const { amount, time } = newCount;
+  app.locals.count.push({ amount, time, id });
+  response.status(201).json({ amount, time, id });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
