384612cb55d0d885f2e28ef2bcb5fc0195994066
Ben Firth <bfirth93@gmail.com>
2020-09-23 10:21:00 -0600

    Add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+var cors = require('cors');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
